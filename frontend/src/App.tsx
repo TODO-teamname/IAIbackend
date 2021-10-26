6dfb6abd29195547c701d3eb411ec536a689f86a
jacob-sahl <jacobsahlmueller@gmail.com>
2021-10-26 02:14:09 -0400

    Added example component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
+import MOOCletView from './Components/MOOCletView/MOOCletView';
 
 function App(): JSX.Element {
   return (
     <BrowserRouter>
       <Switch>
         {/* ADD ROUTES HERE */}
-        <Route path="/" render={() => <h1>Hello World</h1>} />
+        <Route exact path="/" render={() => <h1>Hello World</h1>} />
+        <Route exact path="/mooclet" render={() => <MOOCletView />} />
       </Switch>
     </BrowserRouter>
   );
---
+++ frontend/src/Components/MOOCletView/MOOCletView.tsx
@@ -0,0 +1,12 @@
+import { Component } from 'react';
+import './moocletview.css';
+
+interface Props {}
+
+interface State {}
+
+export default class MOOCletView extends Component<Props, State> {
+  render(): JSX.Element {
+    return <p>MOOCLET</p>;
+  }
+}
