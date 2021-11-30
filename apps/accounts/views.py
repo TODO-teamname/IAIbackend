1a04c4d53a5b5b5efc0a45a1fc5437af5ba29573
Luca Baiocchi <luca.baiocchi@mail.utoronto.ca>
2021-11-30 15:43:16 -0500

    Committing small change to views that was forgot in previous commit

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -9,11 +9,11 @@ class RegisterView(generics.CreateAPIView):
 
     def post(self, request, format='json'):
         serializer = self.get_serializer(data=request.data)
-
-        serializer.is_valid(raise_exception=True)
-
-        user=serializer.save()
-
-        if user:
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-
+        #print(request.data)
+        if serializer.is_valid(raise_exception=True):
+            #print(serializer.errors)
+            user=serializer.save()  
+            if user:
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+            else:    
+                return Response(serializer._errors, status=status.HTTP_400_BAD_REQUEST)
